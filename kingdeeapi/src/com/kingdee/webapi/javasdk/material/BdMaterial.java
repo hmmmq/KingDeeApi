dfac663b1d651054c52f49f4747ccf89b3ea07a6
hmmmq <1053825066@qq.com>
2024-08-18 22:00:14 +0800

    首次提交

---
+++ kingdeeapi/src/com/kingdee/webapi/javasdk/material/BdMaterial.java
@@ -0,0 +1,40 @@
+package com.kingdee.webapi.javasdk.material;
+
+public class BdMaterial {
+    private String FNumber;
+    private String FName;
+    private String FCreateOrgId;
+    private int FUseOrgId;
+
+    public String getFNumber() {
+        return FNumber;
+    }
+
+    public void setFNumber(String FNumber) {
+        this.FNumber = FNumber;
+    }
+
+    public String getFName() {
+        return FName;
+    }
+
+    public void setFName(String FName) {
+        this.FName = FName;
+    }
+
+    public String getFCreateOrgId() {
+        return FCreateOrgId;
+    }
+
+    public void setFCreateOrgId(String FCreateOrgId) {
+        this.FCreateOrgId = FCreateOrgId;
+    }
+
+    public int getFUseOrgId() {
+        return FUseOrgId;
+    }
+
+    public void setFUseOrgId(int FUseOrgId) {
+        this.FUseOrgId = FUseOrgId;
+    }
+}
---
+++ kingdeeapi/src/com/kingdee/webapi/javasdk/material/BdMaterialTest.java
@@ -0,0 +1,68 @@
+package com.kingdee.webapi.javasdk.material;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.kingdee.bos.webapi.entity.IdentifyInfo;
+import com.kingdee.bos.webapi.entity.QueryParam;
+import com.kingdee.bos.webapi.entity.RepoRet;
+import com.kingdee.bos.webapi.sdk.K3CloudApi;
+import com.kingdee.webapi.javasdk.common.SeqHelper;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static org.junit.Assert.fail;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class BdMaterialTest {
+    private static String FNumber = SeqHelper.genNumber("WL");
+    private static String FName = "auwl_" + UUID.randomUUID().toString();
+    private static String groupid = "";
+    private static String materid = "";
+    private static String fileid = "";
+
+    /*本接口用于实现物料 (BD_Material) 的查看功能*/
+    @Test
+    public void ltestviewMaterial() throws Exception {
+        Properties properties = new Properties();
+        File file = new File((new File("./src")).getCanonicalPath() + "/kdwebapi.properties");
+        System.out.println("读取根目录下的配置文件->" + file.getPath());
+        InputStream inputStream = new FileInputStream(file.getPath());
+        properties.load(new InputStreamReader(inputStream, "utf-8"));
+        inputStream.close();
+        IdentifyInfo iden = new IdentifyInfo();
+        System.out.println(iden.toString());
+        iden.setConnectTimeout(Integer.parseInt(properties.getProperty("X-KDApi-ConnectTimeout")));
+        iden.setRequestTimeout(Integer.parseInt(properties.getProperty("X-KDApi-RequestTimeout")));
+        iden.setUserName(properties.getProperty("X-KDApi-UserName"));
+        iden.setAppId(properties.getProperty("X-KDApi-AppID"));
+        iden.setdCID(properties.getProperty("X-KDApi-AcctID"));
+        iden.setAppSecret(properties.getProperty("X-KDApi-AppSec"));
+        iden.setlCID(Integer.parseInt(properties.getProperty("X-KDApi-LCID")));
+        iden.setServerUrl(properties.getProperty("X-KDApi-ServerUrl"));
+        iden.setStockTimeout(Integer.parseInt(properties.getProperty("X-KDApi-StockTimeout")));
+        K3CloudApi api = new K3CloudApi(iden);
+        String data = "{\"CreateOrgId\": 0,\"Number\": "+"\""+FNumber+"\""+",\"Id\": \"\",\"IsSortBySeq\": \"false\"}";
+        String result = api.view("BD_Material", data);
+        Gson gson = new Gson();
+        RepoRet repoRet = gson.fromJson(result, RepoRet.class);
+        if (repoRet.getResult().getResponseStatus().isIsSuccess()) {
+            System.out.printf("物料查看接口: %s%n", result);
+        } else {
+            fail("物料查看接口: " + result);
+        }
+    }
+
+
+}
+
+
+
+
